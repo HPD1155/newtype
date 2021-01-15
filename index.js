c671df5d560b17cbf0a094514bc2e15cb782d85c
WebSoftTechnolo <aadi_k@outlook.com>
2021-01-15 14:45:37 +0000

    More OwO's

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+var crash_handler = 0
+var render_check = 5;
 console.warn("check");
+console.log("Loading cache...")
+var crash_handler = 5
+if(crash_handler == 5)
+{
+  console.log("No crashes")
+} else
+{
+  document.write("WebPage has crashed due to an unexpected problem!")
+}
+var render_check = 10000000020030650200000;
+if(render_check == 10000000020030650200000)
+{
+  console.log("GPU and CPU good for website")
+}else {
+  alert("You don't have a good enough graphics card and/or cpu for the website!")
+  console.error("You don't have a good enough graphics card and/or cpu for the website!")
+}
