8fffa96e6389e2910e18e52f70cfe5b62d8c9389
HPD1155 <aadi_k@outlook.com>
2021-01-20 16:43:49 +0000

    New info.sh and a installation for an other product

---
+++ info.sh
@@ -0,0 +1,12 @@
+echo NewType
+echo ---------------------------------------------------------------------------------
+
+echo THIS IS NEWTYPE INFO LOG
+echo ------------------------------------------------------------------------------------
+echo -
+echo - Go to newtype website: https://hpd1155.github.io/newtype
+echo - Check out the discord on the github page.
+echo -----------------------------------------------------------------------------------
+echo                              more downloads!
+echo - WebCK computer checker in js for html or other
+echo -------------------------------------------------------
---
+++ webCK.sh
@@ -0,0 +1,14 @@
+echo Preparing to install...
+mkdir WebCK
+cd /WebCK
+echo installing...
+git clone https://github.com/HPD1155/WebCK
+echo done!
+echo Add the WebCK.js to your project
+echo use these code:
+echo for only js:
+echo "import (folder)\WebCK.js"
+echo or just keep it in same directory.
+echo for html and js:
+echo "<script src="WebCK"></script>" (Keep in same directory with the js file and html file)
+
