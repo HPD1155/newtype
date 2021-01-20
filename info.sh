6b9277c073f0a9e3c906034c2d57921d578daeb5
HPD1155 <aadi_k@outlook.com>
2021-01-20 16:58:35 +0000

    move things around

--- info.sh
+++ info.sh
@@ -7,7 +7,8 @@ echo -
 echo - Go to newtype website: https://hpd1155.github.io/newtype
 echo - Check out the discord on the github page.
 echo -----------------------------------------------------------------------------------
-echo                              more downloads!
+echo                             download more Products!
+echo go to folder more downloads and cd into there
 echo - WebCK computer checker in js for html or other
 echo -------------------------------------------------------
 echo Linux and Mac version
