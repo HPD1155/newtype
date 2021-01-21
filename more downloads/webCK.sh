ba79a10eb472a44beede18ed8137a29a92084436
HPD1155 <aadi_k@outlook.com>
2021-01-21 14:20:24 +0000

    d

--- more downloads/webCK.sh
+++ more downloads/webCK.sh
@@ -10,5 +10,5 @@ echo for only js:
 echo "import (folder)\WebCK.js"
 echo or just keep it in same directory.
 echo for html and js:
-echo "<script src="WebCK"></script>" (Keep in same directory with the js file and html file)
+echo "<script src="WebCK.js"></script>" (Keep in same directory with the js file and html file)
 
