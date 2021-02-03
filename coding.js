e0772a2519d56e5f18f97e40483ce554f7ab2694
HPD1155 <aadi_k@outlook.com>
2021-02-03 18:05:59 +0000

    added end line - v0.0.7-beta

--- coding.js
+++ coding.js
@@ -3,14 +3,24 @@ function BnR()
   var edtr = document.getElementById("editor").value
   var cnsle = document.getElementById("cnsle").value
   
-  if(edtr.includes("test"))
+  if(edtr.includes("test/"))
   {
     alert("test successful")
   }
-  if(edtr.includes("say"))
+  if(edtr.includes("say/"))
   {
     alert("=> [Function: say]")
   }
-  
-  
+  if(edtr.includes("test.phrase []/"))
+  {
+    alert("Hello")
+  }
+  if(edtr.includes("test.phrase.toCaps []/"))
+  {
+    alert("Hello".toUpperCase())
+  }
+  if(edtr.includes("test.phrase.toLowercase []/"))
+  {
+    alert("Hello".toLowerCase())
+  }
 }
