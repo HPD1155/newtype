da4f7b8efdbb1e56c338e8deb26f619f68febb72
HPD1155 <aadi_k@outlook.com>
2021-02-03 17:53:29 +0000

    v0.0.7-beta

---
+++ coding.js
@@ -0,0 +1,16 @@
+function BnR()
+{
+  var edtr = document.getElementById("editor").value
+  var cnsle = document.getElementById("cnsle").value
+  
+  if(edtr.includes("test"))
+  {
+    alert("test successful")
+  }
+  if(edtr.includes("say"))
+  {
+    alert("=> [Function: say]")
+  }
+  
+  
+}
