5082f9c94f4950c6ee5f77beceebc26a627cec49
HPD1155 <aadi_k@outlook.com>
2021-02-04 00:26:05 +0000

    v0.0.7-beta

--- coding.js
+++ coding.js
@@ -3,27 +3,27 @@ function BnR()
   var edtr = document.getElementById("editor").value
  
   
-  if(edtr.includes("test/"))
+  if(edtr.includes("test|"))
   {
     alert("test successful")
   }
-  if(edtr.includes("say/"))
+  if(edtr.includes("say|"))
   {
     alert("=> [Function: say]")
   }
-  if(edtr.includes("test.phrase []/"))
+  if(edtr.includes("test.phrase []|"))
   {
     alert("Hello")
   }
-  if(edtr.includes("test.phrase.toCaps []/"))
+  if(edtr.includes("test.phrase.toCaps []|"))
   {
     alert("Hello".toUpperCase())
   }
-  if(edtr.includes("test.phrase.toLowercase []/"))
+  if(edtr.includes("test.phrase.toLowercase []|"))
   {
     alert("Hello".toLowerCase())
   }
-  if(edtr.includes("warp/"))
+  if(edtr.includes("warp|"))
   {
     alert("=> [Function: warp]")
   }
